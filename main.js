8a22cb0dbf95f4703f394aa0e2b84377be778a16
Zuzana Volfová <zuzka.volfova@gmail.com>
2022-03-17 10:13:26 +0100

    1.2.3.5.6. done

--- main.js
+++ main.js
@@ -6,24 +6,21 @@
 // Úkol 1 - Založ nové pole se seznamem jmen (alespoň 5)
 // Vypiš jeho obsah do stránky - JavaScript spojí pole automaticky do řetězce
 
-
-
-
+let pole = ['Zuzana', 'Jana', "Marcela", 'Pavla', 'Kamila'];
+let text1 = document.querySelector('#vystup1');
+text1.textContent = pole;
 
 // Úkol 2 - Vypiš třetí jméno v pořadí
 // Nezapomeň, že pole se čísluje od 0
 
-
-
-
-
+let text2 = document.querySelector('#vystup2');
+text2.textContent = pole[2];
 
 // Úkol 3 - Kolik jmen je v poli?
 // Pole má vlastnost pole.length
 
-
-
-
+let text3 = document.querySelector('#vystup3');
+text3.textContent = pole.length;
 
 // Úkol 4 - Vypiš znovu seznam jmen, ALE...
 // Tentokrát použij cyklus .forEach() a před každé jméno přidej
@@ -33,8 +30,10 @@
 // 2. Marie
 // atd.
 
-
-
+let text4 = document.querySelector('#vystup4');
+pole.forEach(function(ele, idx, arr) {
+    console.log = ((idx + 1) + " " + ele)
+});
 
 
 // Úkol 5 - přidej na konec pole další jméno
@@ -42,7 +41,9 @@
 // pole.unshift()
 // Doplněný seznam jmen vypiš (jednoduše jako v příkladu 1)
 
-
+let text5 = document.querySelector('#vystup5');
+pole.push("Klara");
+text5.textContent = pole;
 
 
 
@@ -51,12 +52,14 @@
 // pole.shift()
 // pole.pop()
 
+let text6 = document.querySelector('#vystup6');
+let prvni = pole.shift();
+let posledni = pole.pop();
+text6.textContent = 'prvni vymazane jmeno je ' + prvni + " posledni vymazane jmeno je " +
+    posledni;
 
-
-
-
-
-
+let text7 = document.querySelector('#vystup7');
+text7.textContent = pole
 
 
 
